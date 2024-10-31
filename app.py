402333ad162d53093f2655c557efd9663f4d8d97
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-10-31 07:26:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-pip install opencv-python
-python --version
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-git clone https://github.com/AndEnd-eafit/Deteccion-de-objetos-e-imagenes
-cd C:\Users\Usuario\Downloads\Yolov5-main
-
-wget https://github.com/ultralytics/yolov5/releases/download/v6.1/yolov5s.pt
 #from ultralytics import YOLO
 
 #import sys
